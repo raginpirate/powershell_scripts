b22eb8043ad765163ad4e46fe7e1169315c18574
Daniel Wyckoff <daniel@wyckoff.ca>
2026-05-05 17:55:31 -0400

    init

--- mobile_gamer_mode.ps1
+++ mobile_gamer_mode.ps1
@@ -1,6 +1,8 @@
-Set-DisplayRefreshRate 1 180
-Set-DisplayResolution 1 2560 1600
+$nativeDisplay = Get-DisplayInfo | Where-Object { $_.DisplayName -match "TL134ADXP03" }
+
+Set-DisplayRefreshRate $nativeDisplay.DisplayId 180
+Set-DisplayResolution $nativeDisplay.DisplayId 2560 1600
 powercfg /setacvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMIN 0x0000004B
 powercfg /setdcvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMIN 0x0000004B
 powercfg /setacvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX 0x0000004B
-powercfg /setdcvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX 0x0000004B
+powercfg /setdcvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX 0x0000004B 
